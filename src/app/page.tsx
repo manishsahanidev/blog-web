e812a1dcf63cea1d044a28402d7d346e32715ae5
Manish Sahani <114286890+manishsahani7@users.noreply.github.com>
2024-08-06 19:22:38 +0530

    install shadcn and add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import { MainNav } from "@/components/MainNav";
 import LatestPosts from "@/components/home/LatestPosts";
 
 export default function Home() {
   return (
     <>
-      <LatestPosts />
+      <MainNav />
+      <main>
+        <div>
+          <LatestPosts />
+        </div>
+      </main>
     </>
   );
 }
---
+++ src/components/MainNav.tsx
@@ -0,0 +1,121 @@
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+
+import { cn } from "@/lib/utils"
+// import { Icons } from "@/components/icons"
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+
+const posts: { title: string; href: string; description: string }[] = [
+    {
+        title: "React",
+        href: "/blog/react",
+        description:
+            "Learn React.js and Next.js with our tutorials, guides, and examples.",
+    },
+    {
+        title: "Javascript",
+        href: "/blog/javascript",
+        description:
+            "Learn Javascript with our tutorials, guides, and examples.",
+    },
+    {
+        title: "CSS",
+        href: "/blog/css",
+        description:
+            "Learn CSS with our tutorials, guides, and examples.",
+    },
+    {
+        title: "Java",
+        href: "/blog/java",
+        description:
+            "Learn Java with our tutorials, guides, and examples.",
+    },
+    {
+        title: "Spring Boot",
+        href: "/blog/springboot",
+        description:
+            "Learn SpringBoot with our tutorials, guides, and examples.",
+    },
+    {
+        title: "NodeJS",
+        href: "/blog/nodejs",
+        description:
+            "Learn Node.js and Express.js with our tutorials, guides, and examples.",
+    },
+]
+
+export function MainNav() {
+    return (
+        <div className={cn("flex flex-col items-start md:flex-row md:items-center md:justify-between pt-10 z-50")}>
+            <Link href={"/"}>
+                <div>
+                    {/* Logo Icon */}
+                    <p>Genius Developer</p>
+                </div>
+            </Link>
+            <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <NavigationMenuTrigger>Posts</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                                {posts.map((post) => (
+                                    <ListItem
+                                        key={post.title}
+                                        title={post.title}
+                                        href={post.href}
+                                    >
+                                        {post.description}
+                                    </ListItem>
+                                ))}
+                            </ul>
+                        </NavigationMenuContent>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link href="/about" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                About
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+        </div>
+    )
+}
+
+const ListItem = React.forwardRef<
+    React.ElementRef<"a">,
+    React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+    return (
+        <li>
+            <NavigationMenuLink asChild>
+                <a
+                    ref={ref}
+                    className={cn(
+                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                        className
+                    )}
+                    {...props}
+                >
+                    <div className="text-sm font-medium leading-none">{title}</div>
+                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                        {children}
+                    </p>
+                </a>
+            </NavigationMenuLink>
+        </li>
+    )
+})
+ListItem.displayName = "ListItem"
