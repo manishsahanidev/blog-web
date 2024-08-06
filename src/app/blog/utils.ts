0c476f356be28b174ce0ceb9fff5e7852653487c
Manish Sahani <114286890+manishsahani7@users.noreply.github.com>
2024-08-06 12:03:02 +0530

    blog utils functions added

---
+++ src/app/blog/utils.ts
@@ -0,0 +1,72 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+// get all mdx files in the blog directory
+function getMdxFiles(dir: string) {
+  return fs.readdirSync(dir).filter((file) => path.extname(file) === ".mdx");
+}
+
+// read the content of each file
+function readMdxFile(filePath: fs.PathOrFileDescriptor) {
+  let rawContent = fs.readFileSync(filePath, "utf-8");
+  return matter(rawContent);
+}
+
+// present the mdx data and metadata
+function getMdxData(dir: string) {
+  let mdxFiles = getMdxFiles(dir);
+
+  return mdxFiles.map((file) => {
+    let { data: metadata, content } = readMdxFile(path.join(dir, file));
+    let slug = path.basename(file, path.extname(file));
+
+    return {
+      metadata,
+      slug,
+      content,
+    };
+  });
+}
+
+export function getBlogPosts() {
+  return getMdxData(path.join(process.cwd(), "src", "app", "blog", "contents"));
+}
+
+export function formatDate(date: string, includeRelative = false) {
+  let currentDate = new Date();
+
+  if (!date.includes("T")) {
+    date = `${date}T00:00:00`;
+  }
+
+  let targetDate = new Date(date);
+
+  let yearsAgo = currentDate.getFullYear() - targetDate.getFullYear();
+  let monthsAgo = currentDate.getMonth() - targetDate.getMonth();
+  let daysAgo = currentDate.getDate() - targetDate.getDate();
+
+  let formattedDate = "";
+
+  if (yearsAgo > 0) {
+    formattedDate = `${yearsAgo}y ago`;
+  } else if (monthsAgo > 0) {
+    formattedDate = `${monthsAgo}mo ago`;
+  }
+  if (daysAgo > 0) {
+    formattedDate = `${daysAgo}d ago`;
+  } else {
+    formattedDate = "Today";
+  }
+
+  let fullDate = targetDate.toLocaleString("en-in", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  if (!includeRelative) {
+    return fullDate;
+  }
+  return `${fullDate} (${formattedDate})`;
+}
