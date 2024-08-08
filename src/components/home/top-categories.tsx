1e5df1eddd515dc7c22ee2f4f32eeefe6b21ecff
Manish Sahani <114286890+manishsahani7@users.noreply.github.com>
2024-08-08 20:52:06 +0530

    refactor top category

--- src/components/home/top-categories.tsx
+++ src/components/home/top-categories.tsx
@@ -1,14 +1,15 @@
-import { categories } from "@/lib/placeholder-data"
+// import { categories } from "@/lib/placeholder-data"
 import { Button } from "../ui/button"
 import Link from "next/link"
+import { POSTS } from "@/lib/constants"
 
 export default function TopCategories() {
     return (
         <div className="grid grid-cols-[repeat(auto-fill,minmax(100px,1fr))] gap-2">
-            {categories.map((category) => (
-                <Button key={category} variant={"secondary"} className="hover:scale-110 transition-all" asChild>
-                    <Link href={`/blog/${category}`}>
-                        {category}
+            {POSTS.map((post) => (
+                <Button key={post.title} variant={"secondary"} className="hover:scale-110 transition-all" asChild>
+                    <Link href={`/blog/${post.title}`}>
+                        {post.title}
                     </Link>
                 </Button>
             ))}
