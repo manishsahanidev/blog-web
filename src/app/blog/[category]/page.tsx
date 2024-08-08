8d82e2bdb7a0ca0670a421abc17b0f336048698c
Manish Sahani <114286890+manishsahani7@users.noreply.github.com>
2024-08-08 23:25:56 +0530

    add blog card

---
+++ src/app/blog/[category]/page.tsx
@@ -0,0 +1,43 @@
+import { notFound } from "next/navigation";
+import { getBlogPosts } from "../utils"
+import Link from "next/link";
+import Container from "@/components/Container";
+import CardCategory from "@/components/CardCategory";
+
+export default function Page({ params }: { params: { category: string } }) {
+    let posts = getBlogPosts().filter((post) => post.metadata.category === params.category);
+
+    if (!posts) {
+        notFound();
+    }
+    return (
+        <>
+            <Container>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
+                    {posts
+                        .sort((a, b) => {
+                            if (
+                                new Date(a.metadata.publishedAt) >
+                                new Date(b.metadata.publishedAt)
+                            ) {
+                                return -1;
+                            }
+                            return 1;
+                        })
+                        .map((post) => (
+                            <Link
+                                href={`/blog/${post.metadata.category}/${post.slug}`}
+                                key={post.slug}
+                            >
+                                <CardCategory
+                                    title={post.metadata.title}
+                                    summary={post.metadata.summary}
+                                    date={post.metadata.publishedAt}
+                                />
+                            </Link>
+                        ))}
+                </div>
+            </Container>
+        </>
+    )
+}
--- src/components/home/top-categories.tsx
+++ src/components/home/top-categories.tsx
@@ -7,10 +7,13 @@ export default function TopCategories() {
     return (
         <div className="grid grid-cols-[repeat(auto-fill,minmax(100px,1fr))] gap-2">
             {POSTS.map((post) => (
-                <Button key={post.title} variant={"secondary"} className="hover:scale-110 transition-all" asChild>
-                    <Link href={`/blog/${post.title}`}>
-                        {post.title}
-                    </Link>
+                <Button
+                    key={post.title}
+                    variant={"secondary"}
+                    className="hover:scale-110 transition-all"
+                    asChild
+                >
+                    <Link href={post.href}>{post.title}</Link>
                 </Button>
             ))}
         </div>
